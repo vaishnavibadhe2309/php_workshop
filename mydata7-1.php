2b56166d2423aecc7867ac5c397cfc024790f1f9
vaishnavibadhe2309 <badhev729@gmail.com>
2026-05-07 23:38:15 +0530

    assignment 7

---
+++ mydata7-1.php
@@ -0,0 +1,18 @@
+<?php
+$student = [
+    [
+        "id" => 1,
+        "name" => "avinash"
+    ],
+    [
+        "id" => 2,
+        "name" => "ganesh"
+    ],
+    [
+        "id" => 3,
+        "name" => "tushar"
+    ]
+];
+
+print_r(json_encode($student));
+?>
